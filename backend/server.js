0dede2e976e71d7cc51d387a7318f2791cbce206
lxg39 <2356186697@qq.com>
2025-07-30 10:16:24 +0800

    portfolio services impl

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,23 @@
-const express = require('express');
-// const cors = require('cors');
-const stockRoutes = require('./routes/stock');
+const express = require("express");
+const cors = require("cors"); // 引入 cors 中间件
+const stockRoutes = require("./routes/stock");
+const portfolioRoutes = require("./routes/portfolio");
 
 const app = express();
 const port = 3000;
 
-// 中间件
-// app.use(cors());
+app.use(express.json());
+app.use(cors()); // 使用 cors 中间件解决跨域问题
+
+// 静态文件服务
+// 假设 index.html 文件在 frontend 目录下
+app.use(express.static("../frontend"));
 
 // 路由
-app.use('/api', stockRoutes);
+app.use("/api", stockRoutes);
+app.use("/api", portfolioRoutes);
 
 // 启动服务器
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
